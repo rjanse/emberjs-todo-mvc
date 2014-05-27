1bfa0523f60bb7a59ef4cfe13ec1a5510ea3df39
Robin Janse <r.janse@nedforce.nl>
2014-05-28 00:04:21 +0200

    Deleting a Model

--- js/controllers/todo_controller.js
+++ js/controllers/todo_controller.js
@@ -3,6 +3,7 @@ Todos.TodoController = Ember.ObjectController.extend({
     editTodo: function() {
       this.set('isEditing', true);
     },
+    
     acceptChanges: function() {
       this.set('isEditing', false);
 
@@ -11,6 +12,12 @@ Todos.TodoController = Ember.ObjectController.extend({
       } else {
         this.get('model').save();
       }
+    },
+    
+    removeTodo: function() {
+      var todo = this.get('model');
+      todo.deleteRecord();
+      todo.save();
     }
   },
 
