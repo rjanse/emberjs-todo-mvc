02bdd8439916256f2fd0c299ba792bb24fe57520
Robin Janse <r.janse@nedforce.nl>
2014-05-28 01:07:16 +0200

    Replacing the Fixture Adapter with Another Adapter

--- js/application.js
+++ js/application.js
@@ -1,4 +1,8 @@
 // This will create a new instance of Ember.Application and make it available as a variable named Todos within your browser's JavaScript environment.
 window.Todos = Ember.Application.create();
 
-Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
+// Todos.ApplicationAdapter = DS.FixtureAdapter.extend(); !! No fixures!!
+
+Todos.ApplicationAdapter = DS.LSAdapter.extend({
+  namespace: 'todos-emberjs'
+});
