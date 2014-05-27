473fff08182fc3c6f3431340e920d62dfa07341f
Robin Janse <r.janse@nedforce.nl>
2014-05-28 00:32:13 +0200

    Adding Child Routes

--- js/router.js
+++ js/router.js
@@ -1,10 +1,18 @@
 // This will tell Ember.js to detect when the application's URL matches '/' and to render the todos template.
 Todos.Router.map(function () {
-  this.resource('todos', { path: '/' });
+  this.resource('todos', { path: '/' }, function () {
+    // additional child routes will go here later
+  });
 });
 
 Todos.TodosRoute = Ember.Route.extend({
   model: function () {
     return this.store.find('todo');
+  }
+});
+
+Todos.TodosIndexRoute = Ember.Route.extend({
+  model: function () {
+    return this.modelFor('todos');
   }
 });
